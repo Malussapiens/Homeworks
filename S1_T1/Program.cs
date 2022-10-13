bab9408910c7185a9684fa83d7a8748e1d183069
Dead_Moroz <malussapiens@gmail.com>
2022-10-13 22:33:52 +0300

    first task complete

---
+++ S1_T1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1: Напишите программу, 
+//которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Данная программа принимает на вход 2 числа и сообщает, какое число большее, а какое меньшее.");
+Console.Write("Введите 1-е число:>");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-е число:>");
+int secondNum = int.Parse(Console.ReadLine());
+int min, max;
+if (firstNum > secondNum) { max = firstNum; min = secondNum; }
+else { max = secondNum; min = firstNum; }
+Console.WriteLine($"max={max}, min={min}");
