0da1b8439abaf79e1ee51fdcb0d2c02c4a16e548
Dead_Moroz <malussapiens@gmail.com>
2022-10-13 23:04:02 +0300

    gitignore modified

---
+++ S1_T2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Программа принимает на вход три числа и выдаёт максимальное из этих чисел.");
+int[] numbers = new int[3];     //создаем массив размерностью [3]
+int i = 0;                        //переменная-счетчик
+while (i < numbers.Length)
+{     //заполняем массив
+    Console.Write($"Введите {i + 1}-e число:>");    //последовательно запрашивая
+    numbers[i] = int.Parse(Console.ReadLine());       // у пользователя числа
+    i++;
+}
+
+int max = numbers[0];
+i = 1;
+while (i < numbers.Length)
+{
+    if (max < numbers[i]) { max = numbers[i]; }
+    i++;
+}
+
+Console.WriteLine($"max={max}");
